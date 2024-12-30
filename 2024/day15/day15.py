46609f2d54d2676e9af14025ae1ec5d28c438a27
sambaapu <sambaapu@gmail.com>
2024-12-30 02:55:44 -0500

    day15

---
+++ 2024/day15/day15.py
@@ -0,0 +1,88 @@
+class Grid:
+    def __init__(self, filepath):
+        self.grid, self.dirs = self.parse_input_file(filepath)
+        self.robot_row, self.robot_col = self.get_initial_robot_pos()
+    
+    def parse_input_file(self, filepath):
+        with open(filepath, 'r') as f:
+            content = f.read()
+            parts = content.split('\n\n')
+            gridStr = parts[0].split('\n')
+            grid = []
+            for i in range(len(gridStr)):
+                grid.append(list(gridStr[i]))
+            dirs = parts[1]
+            dirs.replace('\n', '')
+            print(type(dirs[0]))
+        return grid, dirs
+    
+    def get_initial_robot_pos(self):
+        for i in range(len(self.grid)):
+            for j in range(len(self.grid[i])):
+                if self.grid[i][j] == '@': 
+                    return [i, j]
+    def update_pixel(self, new_row, new_col, row, col):
+        self.grid[new_row][new_col] = self.grid[row][col]
+        self.grid[row][col] = '.'
+
+    def update_robot_pos(self, new_row, new_col):
+        self.robot_row = new_row
+        self.robot_col = new_col
+    
+    def update(self):
+        for dir in self.dirs:
+            if dir not in {'^', 'v', '>', '<'}:
+                continue
+            new_row, new_col = self.get_new_pos(self.robot_row, self.robot_col, dir)
+            if self.grid[new_row][new_col] == '#':
+                continue
+            if self.grid[new_row][new_col] == '.':
+                self.update_pixel(new_row, new_col, self.robot_row, self.robot_col)
+                self.update_robot_pos(new_row, new_col)
+
+            if self.grid[new_row][new_col] == 'O':
+                while self.grid[new_row][new_col] == 'O':
+                    new_row, new_col = self.get_new_pos(new_row, new_col, dir)
+                if self.grid[new_row][new_col] == '#':
+                    continue
+                while (new_row, new_col) != (self.robot_row, self.robot_col):
+                    new_row, new_col = self.get_old_pos(new_row, new_col, dir)
+                    r, c = self.get_new_pos(new_row, new_col, dir)
+                    self.update_pixel(new_row, new_col, r, c)
+                r, c = self.get_new_pos(self.robot_row, self.robot_col, dir)
+                self.update_robot_pos(r, c)
+    
+    def get_new_pos(self, row, col, dir):
+        if dir == '^':
+            row -= 1
+        elif dir == 'v':
+            row += 1
+        elif dir == '>':
+            col += 1
+        elif dir == '<':
+            col -= 1
+        return row, col
+    
+    def get_old_pos(self, row, col, dir):
+        if dir == '^':
+            row += 1
+        elif dir == 'v':
+            row -= 1
+        elif dir == '>':
+            col -= 1
+        elif dir == '<':
+            col += 1
+        return row, col
+
+    def print_grid(self):
+        for i in range(len(self.grid)):
+            print(''.join(self.grid[i]))
+    def print_dirs(self):
+        print(self.dirs)
+
+g = Grid('input1.txt')
+g.print_grid()
+print("")
+g.dirs = '^^'
+g.update()
+g.print_grid()
